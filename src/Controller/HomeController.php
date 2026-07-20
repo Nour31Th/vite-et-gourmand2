0fa9a389b4942b9f03174a96b68e8522f89fcc77
miorath <thualmiora.31@gmail.com>
2026-07-20 19:20:33 +0200

    feat:add home page (sections) et css, newsletter csrf

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,19 +1,26 @@
 <?php
 
-//gestion page d'accueil publique, Route : GET / (racine du site)//
+//gestion page d'accueil publique, variable : avis validés, horaires footer// 
 
 namespace App\Controller;
 
+use App\Repository\AvisRepository;
+use App\Repository\HoraireRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
-    {
-        return $this->render('home/index.html.twig');
+    public function index(
+        AvisRepository $avisRepo,
+        HoraireRepository $horaireRepo
+    ): Response {
+        return $this->render('home/index.html.twig', [
+            'avis'          => $avisRepo->findBy(['valide' => true], ['dateAvis' => 'DESC'], 3),  //avis validés
+            'horaires'      => $horaireRepo->findAll(),  //horaires pr footer dynmq
+        ]);
     }
 }
+
